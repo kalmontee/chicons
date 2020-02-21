5decc4457e5f12218c4a591471aa6cbb079a9e35
Felix Fernandez <fernandezfelix460.gmail.com>
2020-02-20 20:27:51 -0500

    added pages, form and jumbotron components, and did some light styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from "./components/NavBar/NavBar" ;
 import SignUp from "./components/Login/SignUp";
 import SignIn from "./components/Login/SignIn";
+import Poster from "./pages/Poster"
+import Seeker from "./pages/Seeker"
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        <h1>Lets get to work!!!!</h1>
-        <h3>Post your apartments</h3>
-
+        <Switch>
         <Route exact path="/sign-up" component={SignUp} />
         <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/apartments" component={Poster} />
+        <Route exact path="/api" component={Seeker}/>
+        </Switch>
       </div>
     </Router>
   );
---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./jumbotron.css";
+
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Apartamentos</h1>
+          <p className="lead">Post and Look for best spaces in your town</p>
+        </div>
+      </div>
+    )
+ }
+
+ export default Jumbotron
